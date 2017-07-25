2b16aa134553a5e87aac2a754ceb73783068d811
Maryanne Audran <maryanne.audran@gmail.com>
2017-07-25 15:56:13 +0200

    - Show data from contact in tableview.php

--- view/tableView.php
+++ view/tableView.php
@@ -6,6 +6,11 @@
 
 <div id="table-wrapper">
     <div id="table-container">
+
+
+
+
+
         <?php
         $mysqli = new mysqli("localhost", "root", "", "rmn-project");
 
@@ -15,8 +20,34 @@
         }
 
 
-        $data = $mysqli->query("SELECT * FROM contact");
+        $request = $mysqli->query("SELECT * FROM contact");
+        $data = array();
+
+
+        echo '<table class="data-table">
+        <tr class="data-heading">';  //initialize table tag
+        while ($property = mysqli_fetch_field($request)) {
+            echo '<td>' . $property->name . '</td>';  //get field name for header
+            array_push($data, $property->name);  //save those to array
+        }
+        echo '</tr>'; //end tr tag
+
+        //showing all data
+        while ($row = mysqli_fetch_array($request)) {
+            echo "<tr>";
+            foreach ($data as $item) {
+                echo '<td>' . $row[$item] . '</td>'; //get items using property value
+            }
+            echo '</tr>';
+        }
+        echo "</table>";
+
+
+
         ?>
+
+
+
 
 
 
